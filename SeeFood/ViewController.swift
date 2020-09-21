c49cb63a0e33b7c965980b656febc9a4fa8c1586
Marcus Kim <marcuskim1989@gmail.com>
2020-09-21 16:25:12 -0700

    Initial Commit

---
+++ SeeFood/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  SeeFood
+//
+//  Created by Marcus Y. Kim on 9/21/20.
+//
+
+import UIKit
+import CoreML
+import Vision
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
